07c5b5b04322c93c2c84f968db9c4aba63e910e6
unknown <izazahmed0177@gmail.com>
2022-11-08 19:12:06 +0600

    index server rume

--- .history/index_20221108190250.js
+++ .history/index_20221108190250.js
@@ -1,15 +1,11 @@
 const express = require('express')
 const app = express()
-var cors = require('cors')
 const port = process.env.PORT || 5000
 
-
-app.use(cors())
-
 app.get('/', (req, res) => {
-    res.send('Cake node server running')
+    res.send('Simple node server running')
 })
 
 app.listen(port, () => {
-    console.log(`Cake node server listening on port ${port}`)
+    console.log(`Simple node server listening on port ${port}`)
 })
--- .history/index_20221108190342.js
+++ .history/index_20221108190342.js
@@ -1,10 +1,6 @@
 const express = require('express')
 const app = express()
-var cors = require('cors')
 const port = process.env.PORT || 5000
-
-
-app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Cake node server running')
